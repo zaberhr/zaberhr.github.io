19ba6c58e53fd08be1bd7b6555f74491d4927706
zaberhr <zaberhr@gmail.com>
2017-08-05 21:50:13 +0600

    Class-16

---
+++ Class-16/assets/js/main.js
@@ -0,0 +1,31 @@
+jQuery(document).ready(function($){
+  
+    $(".profilo-filter li").on('click', function() {
+
+    	var filterData = $(this).attr("data-filter");
+
+    	$(".portfolio-list").isotope({
+    		filter: filterData
+
+    	});
+
+    	$(".profilo-filter li").removeClass('active');
+    	$(this).addClass('active');
+    });
+    
+    $(".portfolio-list").isotope({
+    	itemSelector: '.single-portfolio-item',
+    	percentPosition: true,
+    	masonry: {
+    		columnWidth: '.single-portfolio-item',
+    		horizontalOrder: true
+    	}
+    });
+
+    $("body").perfectScrollbar();
+    $(".scrolled-another").perfectScrollbar({
+        theme: 'yellow-theme'
+    });
+
+});
+
